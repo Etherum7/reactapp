80009bfbae048dfe4c5dc4feb84eb088ff561264
Etherum7 <hvs.231918@gmail.com>
2020-06-11 15:04:32 +0530

    Updates

---
+++ precache-manifest.760ca1d4052ffa5e33ddb2bf633554d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4d1fdc9a8b36cb766682fd2871799a",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "f7961911c5db216054c1",
+    "url": "/reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3805b02eb4036d9f3fb9",
+    "url": "/reactapp/static/js/2.e7402e6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.e7402e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7961911c5db216054c1",
+    "url": "/reactapp/static/js/main.33cb62e0.chunk.js"
+  },
+  {
+    "revision": "2bf76cee745d9be4ddbe",
+    "url": "/reactapp/static/js/runtime-main.03298fc8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
